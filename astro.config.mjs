cfcec261acb4e0600e8d81023ff931d046c2f3cd
dnovak_home <dnovak@student.42.fr>
2025-11-20 13:40:46 +0100

    Deploying scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://novakd14.github.io/Froggy-Blog',
+
   srcDir: './web',
 
   vite: {
