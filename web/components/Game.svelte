1e47c223c7ba7145eaf0ca2855825178eb35158a
dnovak_home <dnovak@student.42.fr>
2025-11-28 10:53:22 +0100

    start of the game

--- web/components/Game.svelte
+++ web/components/Game.svelte
@@ -1,3 +1,35 @@
-<div class="w-9/10 max-w-150 aspect-square m-auto border-2 rounded-xl mt-10">
-	<p class="text-center">Tady bude hra! :)</p>
+<script>
+	const srcFly = "/Froggy-Blog/fly.png";
+	const srcFrog = "/Froggy-Blog/frog.png";
+	const srcLily = "/Froggy-Blog/lily.png";
+	const srcReeds = "/Froggy-Blog/reeds.png";
+
+	const cardImages = [
+		{ src: srcFly },
+		{ src: srcFrog },
+		{ src: srcLily },
+		{ src: srcReeds },
+	];
+
+	let cards = [...cardImages, ...cardImages];
+
+	const shuffleCards = () => {
+		console.log(cardImages);
+	};
+</script>
+
+<div class="w-9/10 max-w-150 m-auto border-2 rounded-xl mt-10">
+	<div class="w-min m-auto">
+		<button
+			class="text-center whitespace-nowrap font-bold p-2 pt-1 pb-1 border-2 rounded-xl mt-2 bg-[#5dab91] hover:bg-[#378f71] cursor-pointer"
+			on:click={shuffleCards}
+		>
+			Nová hra
+		</button>
+	</div>
+	<div class="grid grid-cols-4 gap-4 m-4">
+		{#each cards as card}
+			<img src={card.src} alt="Obrázek" class="border-2 rounded-xl" />
+		{/each}
+	</div>
 </div>
